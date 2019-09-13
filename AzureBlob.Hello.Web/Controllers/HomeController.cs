d7bb7a511a9084e79495a613a9c12f5b017d7120
Anshu Bhatnagar <ansh.bhatnagar@gmail.com>
2019-09-13 18:29:21 +0530

    Initial Commit

---
+++ AzureBlob.Hello.Web/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AzureBlob.Hello.Web.Models;
+using Microsoft.AspNetCore.Http;
+using AzureBlob.Hello.Web.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureBlob.Hello.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            string connectionString = _config.GetValue<string>("AzureStorage:ConnectionString");
+
+            foreach (var file in files)
+            {
+                if (file.Length <= 0)
+                {
+                    continue;
+                }
+
+                using (var stream = file.OpenReadStream())
+                {
+                    BlobService blobService = new BlobService();
+                    await blobService.UploadBlob(file.FileName, stream, connectionString);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureBlob.Hello.Web/Services/BlobService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBlob.Hello.Web.Services
+{
+    public class BlobService
+    {
+        public async Task UploadBlob(string fileName, Stream stream, string connectionString)
+        {
+            CloudStorageAccount storageAccount;
+
+            if (!CloudStorageAccount.TryParse(connectionString, out storageAccount))
+            {
+                throw new InvalidOperationException("Unable to connect Azure Storage Account.");
+            }
+
+            // Create the CloudBlobClient that represents the 
+            // Blob storage endpoint for the storage account.
+            CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+            // Create a container called 'quickstartblobs' and 
+            // append a GUID value to it to make the name unique.
+            CloudBlobContainer cloudBlobContainer =
+                cloudBlobClient.GetContainerReference("imagesblob");
+
+            if (!await cloudBlobContainer.ExistsAsync())
+            {
+                await cloudBlobContainer.CreateAsync();
+
+                // Set the permissions so the blobs are public.
+                BlobContainerPermissions permissions = new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                };
+                await cloudBlobContainer.SetPermissionsAsync(permissions);
+            }
+
+            // Get a reference to the blob address, then upload the file to the blob.
+            // Use the value of localFileName for the blob name.
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+
+            await cloudBlockBlob.UploadFromStreamAsync(stream);
+        }
+    }
+}
